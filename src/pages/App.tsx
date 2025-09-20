693cec6654ad4d109dbcf90393dd68ad82a4d1e3
Asaduzzaman14 <asadasad9282@gmail.com>
2025-09-20 10:21:51 +0600

    update

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -6,12 +6,11 @@ import Popups from '../components/Popups'
 import Web3ReactManager from '../components/Web3ReactManager'
 import AddLiquidity from './AddLiquidity'
 import { RedirectDuplicateTokenIds, RedirectOldAddLiquidityPathStructure } from './AddLiquidity/redirects'
+import Home from './Home/Home'
 import Pool from './Pool'
 import PoolFinder from './PoolFinder'
 import RemoveLiquidity from './RemoveLiquidity'
 import { RedirectOldRemoveLiquidityPathStructure } from './RemoveLiquidity/redirects'
-import Swap from './Swap'
-import { RedirectPathToSwapOnly } from './Swap/redirects'
 
 const AppWrapper = styled.div`
   display: flex;
@@ -55,27 +54,29 @@ export default function App() {
   return (
     <Suspense fallback={null}>
       <HashRouter>
-        <AppWrapper>
-          <Menu>
-            <BodyWrapper>
-              <Popups />
-              <Web3ReactManager>
-                <Switch>
-                  <Route exact path="/swap" component={Swap} />
-                  <Route exact path="/find" component={PoolFinder} />
-                  <Route exact path="/pool" component={Pool} />
-                  <Route exact path="/add" component={AddLiquidity} />
-                  <Route exact path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
-                  <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
-                  <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
-                  <Route exact path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
-                  <Route component={RedirectPathToSwapOnly} />
-                </Switch>
-              </Web3ReactManager>
-              <Marginer />
-            </BodyWrapper>
-          </Menu>
-        </AppWrapper>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <AppWrapper>
+            <Menu>
+              <BodyWrapper>
+                <Popups />
+                <Web3ReactManager>
+                  <Switch>
+                    <Route exact path="/find" component={PoolFinder} />
+                    <Route exact path="/pool" component={Pool} />
+                    <Route exact path="/add" component={AddLiquidity} />
+                    <Route exact path="/remove/:currencyIdA/:currencyIdB" component={RemoveLiquidity} />
+                    <Route exact path="/add/:currencyIdA" component={RedirectOldAddLiquidityPathStructure} />
+                    <Route exact path="/add/:currencyIdA/:currencyIdB" component={RedirectDuplicateTokenIds} />
+                    <Route exact path="/remove/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
+                    {/* <Route component={RedirectPathToSwapOnly} /> */}
+                  </Switch>
+                </Web3ReactManager>
+                <Marginer />
+              </BodyWrapper>
+            </Menu>
+          </AppWrapper>
+        </Switch>
       </HashRouter>
     </Suspense>
   )
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>Hello mindchain</h2>
+      <Link to="/pool">Swap</Link>
+    </div>
+  )
+}
+
+export default Home
